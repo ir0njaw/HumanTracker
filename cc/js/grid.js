f61d536116cb4ea58bb8743de4b7ebda97ff41e4
Nikita <kelesis.nikita@gmail.com>
2017-11-27 15:37:25 +0300

    Update grid.js

--- cc/js/grid.js
+++ cc/js/grid.js
@@ -348,7 +348,7 @@ var Grid = (function() {
 			this.$details = $( '<div class="og-details"></div>' ).append( this.$title, this.$description, this.$href );
 			this.$loading = $( '<div class="og-loading"></div>' );
 			this.$fullimage = $( '<div class="og-fullimg"></div>' ).append( this.$loading );
-			this.$closePreview = $( '<span class="og-close"></span>' );
+			
 			this.$previewInner = $( '<div class="og-expander-inner"></div>' ).append( this.$closePreview, this.$fullimage, this.$details );
 			this.$previewEl = $( '<div class="og-expander"></div>' ).append( this.$previewInner );
 			// append preview element to the item
