1282ad3d493f030d15e37da1f512ca6ff3e90b8f
Nikita <kelesis.nikita@gmail.com>
2017-11-29 00:58:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,12 +16,13 @@ mysql -u $dbuser -p$dbpassword dbcc < dbcc.sql
 rm /var/www/html/admin/install.sh
 rm /var/www/html/admin/dbcc.sql
 sudo chmod -R 757 /var/www
-read -p "If the password from the database != 'root' then you need to change config file, ok?" -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
+echo ""
+read -r -p "If the password from the database != 'root' then you need to change config file, ok? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
 then
     nano /var/www/html/admin/cc/bd.php
 fi
+
 
 echo "FuckHumans has been successfully installed!!!"
 
