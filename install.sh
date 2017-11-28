f921025b22814b559d1e768fddba8bbe979bf7c7
Nikita <kelesis.nikita@gmail.com>
2017-11-28 22:50:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo apt-get -y install php7.0-zip
 sudo a2enmod rewrite 
 sudo service apache2 restart
 echo 'q:$apr1$4y1jPCH.$oNzkDf/th.BOzQ3DbpWfM/' > /var/www/.htpasswd
-mv EasySocial /var/www/html/admin && cd /var/www/html/admin  
+cd ../ && mv EasySocial /var/www/html/admin && cd /var/www/html/admin  
 echo "ENTER DATABASE USER NAME:"
 read dbuser
 echo "ENTER DATASE PASSWORD:"
