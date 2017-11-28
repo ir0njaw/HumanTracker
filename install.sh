40cb3a7e0097a2e423edcb0e232024703182bf2d
Nikita <kelesis.nikita@gmail.com>
2017-11-29 02:33:32 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo apt-get -y install php7.0-zip
 sudo a2enmod rewrite 
 sudo service apache2 restart
 echo 'q:$apr1$4y1jPCH.$oNzkDf/th.BOzQ3DbpWfM/' > /var/www/.htpasswd
-cd ../ && mv EasySocial /var/www/html/admin && cd /var/www/html/admin  
+cd ../ && mv EasySocial /var/www/html/admin_cc && cd /var/www/html/admin_cc  
 echo "///////////////////////////////"
 echo "ENTER DATABASE USER NAME:"
 echo "///////////////////////////////"
@@ -16,14 +16,14 @@ read dbpassword
 mysql -u $dbuser -p$dbpassword -e "CREATE DATABASE dbcc"
 echo "CREATE DATASE dbcc -> Done"
 mysql -u $dbuser -p$dbpassword dbcc < dbcc.sql
-rm /var/www/html/admin/install.sh
-rm /var/www/html/admin/dbcc.sql
+rm /var/www/html/admin_cc/install.sh
+rm /var/www/html/admin_cc/dbcc.sql
 sudo chmod -R 757 /var/www
 echo "///////////////////////////////"
 read -r -p "If the password from the database != 'root' then you need to change config file, ok? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
 then
-    nano /var/www/html/admin/cc/bd.php
+    nano /var/www/html/admin_cc/cc/bd.php
 fi
 echo "///////////////////////////////"
 echo ""
