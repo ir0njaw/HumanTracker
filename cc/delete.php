1f7b7551eab09a5a51838427cefe25da42b42f21
Nikita <kelesis.nikita@gmail.com>
2017-12-01 17:37:05 +0300

    Update delete.php

--- cc/delete.php
+++ cc/delete.php
@@ -3,26 +3,26 @@ include ('bd.php');
 if(isset($_GET['id'])){$id = $_GET['id'];}
 if(isset($_GET['user'])){$client_id = $_GET['user'];}
 
-if($id =='1'){	
-	$query = mysqli_query($link, "DELETE FROM tasks WHERE client_id=$client_id");
-	header('Location: http://localhost/admin/cc/hta/');
+if($id =='1'){
+        $query = mysqli_query($link, "DELETE FROM tasks WHERE client_id=$client_id");
+        header('Location: ../hta/');
 }
 elseif($id =='2'){
-	$query = mysqli_query($link, "TRUNCATE logs_visited");
-	header('Location: http://localhost/admin/cc/phishing/');
+        $query = mysqli_query($link, "TRUNCATE logs_visited");
+        header('Location: phishing/');
 }
 elseif($id =='3'){
-	$query = mysqli_query ($link, "TRUNCATE logs_phishing");
-	header('Location: http://localhost/admin/cc/phishing/');
+        $query = mysqli_query ($link, "TRUNCATE logs_phishing");
+        header('Location: phishing/');
 }
 elseif($id =='4'){
-	$query = mysqli_query ($link, "TRUNCATE logs_makros");
-	header('Location: http://localhost/admin/cc/makros/');
+        $query = mysqli_query ($link, "TRUNCATE logs_makros");
+        header('Location: makros/');
 }
 
 elseif($id =='5'){
-	$query = mysqli_query ($link, "TRUNCATE logs_common");
-	header('Location: http://localhost/admin/cc/report/');
+        $query = mysqli_query ($link, "TRUNCATE logs_common");
+        header('Location: report/statistic.php');
 }
 
 ?>
