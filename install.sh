7b4a5df14e4603bb0e4f01189f7ab88c9a4e1aa9
Nikita <kelesis.nikita@gmail.com>
2017-12-07 23:10:55 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ apt-get -y install python-dev python-setuptools
 apt-get -y install libtiff5-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python-tk
 pip2 install openpyxl
 pip2 install pillow
+sudo dpkg-reconfigure tzdata
 
 echo "///////////////////////////////"
 echo "Installation of Mailinabox"
