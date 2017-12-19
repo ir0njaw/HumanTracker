2c0e3f9e67ae38eb740ab7212bb806c83f172855
Nikita <kelesis.nikita@gmail.com>
2017-12-19 13:54:11 +0300

    Update index.php

--- cc/template/index.php
+++ cc/template/index.php
@@ -86,6 +86,9 @@
                                 <a href="../template/">Готовые</a>
                             </li>
                             <li>
+                                <a href="../template/deployed.php">Развернутые шаблоны</a>
+                            </li>
+                            <li>
                                 <a href="../template/new.php">Загрузить новый</a>
                             </li>
                             <li>
