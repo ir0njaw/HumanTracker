d0b420f0ebc84cd62411f1c73c21dbeaee1d2a24
Nikita <kelesis.nikita@gmail.com>
2019-08-28 00:10:53 +0300

    Update dbcc.sql

--- dbcc.sql
+++ dbcc.sql
@@ -1,7 +1,7 @@
 /*
  Navicat Premium Data Transfer
 
-
+ Source Server         : mr-qroup.ru
  Source Server Type    : MySQL
  Source Server Version : 50727
  Source Host           : localhost:3306
@@ -11,7 +11,7 @@
  Target Server Version : 50727
  File Encoding         : 65001
 
- Date: 13/08/2019 01:34:45
+ Date: 28/08/2019 00:08:06
 */
 
 SET NAMES utf8mb4;
@@ -25,9 +25,9 @@ CREATE TABLE `attacks_stats` (
   `attack_name` varchar(255) CHARACTER SET cp1251 COLLATE cp1251_bin NOT NULL,
   `count` varchar(255) CHARACTER SET cp1251 COLLATE cp1251_bin NOT NULL,
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `description` text CHARACTER SET cp1251 COLLATE cp1251_bin NOT NULL,
+  `description` longtext,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=94 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for deployed
@@ -46,8 +46,7 @@ CREATE TABLE `logs_common` (
   `id` varchar(255) NOT NULL,
   `attack` varchar(255) NOT NULL,
   `first_stage` varchar(255) NOT NULL,
-  `second_stage` varchar(255) NOT NULL,
-  `third_stage` varchar(255) NOT NULL
+  `second_stage` varchar(255) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -115,7 +114,7 @@ CREATE TABLE `tasks` (
   `argument_human` varchar(255) NOT NULL,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for template
@@ -133,16 +132,13 @@ CREATE TABLE `template` (
 -- Records of template
 -- ----------------------------
 BEGIN;
-INSERT INTO `template` VALUES ('Яндекс.Диск.jpg', 'Яндекс.Диск_logo.jpg', 'Яндекс.Диск.zip', 'Яндекс.Диск/', '');
 INSERT INTO `template` VALUES ('Обновление VPN.jpg', 'Обновление VPN_logo.jpg', 'Обновление VPN.zip', 'Обновление VPN/', '');
-INSERT INTO `template` VALUES ('Outlook.jpg', 'Outlook_logo.jpg', 'Outlook.zip', 'Outlook/', '');
-INSERT INTO `template` VALUES ('Яндекс.Паспорт.jpg', 'Яндекс.Паспорт_logo.jpg', 'Яндекс.Паспорт.zip', 'Яндекс.Паспорт/', '');
+INSERT INTO `template` VALUES ('Outlook.jpg', 'Outlook_logo.jpg', 'Outlook.zip', 'Outlook/', 'Стандартный шаблон Outlook');
 INSERT INTO `template` VALUES ('Проверка пароля.jpg', 'Проверка пароля_logo.jpg', 'Проверка пароля.zip', 'Проверка пароля/', '');
 INSERT INTO `template` VALUES ('Вебинар.jpg', 'Вебинар_logo.jpg', 'Вебинар.zip', 'Вебинар/', '');
 INSERT INTO `template` VALUES ('Установка антивируса.jpg', 'Установка антивируса_logo.jpg', 'Установка антивируса.zip', 'Установка антивируса/', '');
-INSERT INTO `template` VALUES ('Премия.jpg', 'Премия_logo.jpg', 'Премия.zip', 'Премия/', '');
 INSERT INTO `template` VALUES ('Kerio.jpg', 'Kerio_logo.jpg', 'Kerio.zip', 'Kerio/', 'Kerio Connect');
-INSERT INTO `template` VALUES ('Перерасчет ЗП.jpg', 'Перерасчет ЗП_logo.jpg', 'Перерасчет ЗП.zip', 'Перерасчет ЗП/', '');
+INSERT INTO `template` VALUES ('Перерасчет ЗП.jpg', 'Перерасчет ЗП_logo.jpg', 'Перерасчет ЗП.zip', 'Перерасчет ЗП/', 'Макросы');
 COMMIT;
 
 -- ----------------------------
