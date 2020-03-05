0a9e8e2eef493fe42c8bee00e0e1800dc6617d33
Nikita <kelesis.nikita@gmail.com>
2020-03-05 17:43:29 +0300

    Update delete.php

--- cc/delete.php
+++ cc/delete.php
@@ -1,10 +1,14 @@
 <?php
 include ('bd.php');
+mysqli_set_charset($link, "utf8");
 if(isset($_GET['id'])){$id = $_GET['id'];}
 if(isset($_GET['user'])){$client_id = $_GET['user'];}
 
-if($id =='1'){
-        $query = mysqli_query($link, "DELETE FROM tasks WHERE client_id='$client_id'");
+if($id =='hta'){
+        $query = mysqli_query ($link, "TRUNCATE replies");
+        $query = mysqli_query ($link, "TRUNCATE tasks");
+        $query = mysqli_query ($link, "TRUNCATE timelog");
+        $query = mysqli_query ($link, "UPDATE attacks_stats SET count='0' WHERE attack_name='Вебинар'");
         header('Location: hta/');
 }
 elseif($id =='2'){
@@ -13,10 +17,12 @@ elseif($id =='2'){
 }
 elseif($id =='3'){
         $query = mysqli_query ($link, "TRUNCATE logs_phishing");
+        $query = mysqli_query ($link, "UPDATE attacks_stats SET count='0' WHERE (attack_name='Outlook' OR attack_name='Проверка пароля' OR attack_name='Kerio')");
         header('Location: phishing/');
 }
 elseif($id =='4'){
         $query = mysqli_query ($link, "TRUNCATE logs_makros");
+        $query = mysqli_query ($link, "UPDATE attacks_stats SET count='0' WHERE attack_name='Перерасчет ЗП'");
         header('Location: makros/');
 }
 
@@ -24,5 +30,4 @@ elseif($id =='5'){
         $query = mysqli_query ($link, "TRUNCATE logs_common");
         header('Location: report/statistic.php');
 }
-
 ?>
